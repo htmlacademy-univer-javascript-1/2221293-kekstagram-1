bc69ec9f44e9b368c471902f5472659886d3f1ed
KitnissPrior <kittrisprior@gmail.com>
2022-12-04 16:07:53 +0500

    Взаимодействие с сервером

--- js/util.js
+++ js/util.js
@@ -1,7 +1,3 @@
-import { getRandomNumber } from './random-number.js';
-
-const getRandomElement = (array) => array[getRandomNumber(0, array.length - 1)];
-
 const isEscape = (evt) => evt.key === 'Escape';
 
-export {getRandomElement, isEscape};
+export {isEscape};
